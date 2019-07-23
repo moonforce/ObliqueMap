8c9ea82f395b38af022b2426c1ea0b450a2259a8
liuyue <liuyue19891018@163.com>
2019-07-23 16:27:32 +0800

    增加设置面板，添加了Photoshop路径的设置

---
+++ Assets/scripts/SettingsPanelCtrl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Crosstales.FB;
+
+public class SettingsPanelCtrl : MonoBehaviour
+{
+    private TMP_InputField m_PhotoshopInputField;
+
+    void Start()
+    {
+        m_PhotoshopInputField = transform.Find("Photoshop/InputField").GetComponent<TMP_InputField>();
+        transform.Find("Photoshop/SetButton").GetComponent<Button>().onClick.AddListener(SetPhotoshopPath);
+    }
+
+    void SetPhotoshopPath()
+    {
+        string folderPath = FileBrowser.OpenSingleFile("选择Photoshop路径", null, "exe");
+        if (folderPath.Length == 0)
+            return;
+        m_PhotoshopInputField.text = folderPath;
+    }
+
+    public void OpenSettingsPanel()
+    {
+        gameObject.SetActive(true);
+    }
+}
