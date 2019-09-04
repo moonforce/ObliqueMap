53722c552dab754cd301f7afb235aa63949cd543
liuyue <liuyue19891018@163.com>
2019-09-04 10:49:30 +0800

    添加打开文档的功能

---
+++ Assets/scripts/Helps.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helps : MonoBehaviour
+{
+    private string m_HelpDocumentPath = Application.streamingAssetsPath + "/UserManual.docx";
+
+    void Start()
+    {
+        
+    }
+
+    public void OpenUserManual()
+    {
+        Application.OpenURL(m_HelpDocumentPath);
+    }
+}
